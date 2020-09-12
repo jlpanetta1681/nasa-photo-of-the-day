c46bab9d6391595c91eb01b5b16ea08426be8874
Joe-Panetta <jlpanetta1681@icloud.com>
2020-09-12 02:12:59 -0400

    started my redo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+     <h2>New App</h2>
     </div>
   );
 }
